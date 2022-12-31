08d6e5c9b5b01a2c36a6e71728f167a78dc33116
charan1225 <118468582+charan1225@users.noreply.github.com>
2022-12-31 16:33:25 +0530

    ravi

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ region = "ap-south-1"
 access_key = "AKIATNHGTLCAQWXOKFPU"
 secret_key = " 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"
 }
-
 resource "aws_instance" "one" {
 ami = "ami-0cca134ec43cf708f"
 instance_type = "t2.micro"
