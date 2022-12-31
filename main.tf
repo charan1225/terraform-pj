fbb63303721cec136a2c52c9de9d17fcea0826ea
charan1225 <118468582+charan1225@users.noreply.github.com>
2022-12-31 16:30:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-resion = "ap-south-1"
+region = "ap-south-1"
 access_key = "AKIATNHGTLCAQWXOKFPU"
 secret_key = " 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"
 }
@@ -8,6 +8,6 @@ resource "aws_instance" "one" {
 ami = "ami-0cca134ec43cf708f"
 instance_type = "t2.micro"
 tags = {
-name = "charan"
+Name = "charan"
 }
 }
